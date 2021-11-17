0dbffc5adfbb0cc899004b43b63709a9e8941c12
Luiz Martins <luizmartins.prog@gmail.com>
2021-11-17 20:02:00 -0300

    add error middleware

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', require('./routes/userRoutes'))
-
+app.use(require('./middlewares/errors'))
 
 app.listen(PORT, () => {
     console.log(`app listen to localhost ${PORT}`);
