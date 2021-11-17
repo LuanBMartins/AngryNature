49091f45198789400ea59a3627d31fc1e89e7898
Luiz Martins <luizmartins.prog@gmail.com>
2021-11-17 19:18:35 -0300

    add database connect

---
+++ server/infra/database.js
@@ -0,0 +1,21 @@
+const config = require('config')
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize(
+	config.get('postgres.petshop'),
+	config.get('postgres.postgres'),
+	config.get('postgres.senha'),
+	{
+		host: 'localhost',
+		dialect: 'postgres',
+		logging: false,
+	},
+)
+
+sequelize.authenticate().then(() => {
+    console.log('Conexão com banco estabelecida com sucesso!');
+});
+
+module.exports = sequelize
+
+
